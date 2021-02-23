930fb075dcbd9154a53b606e0664c24c34181daf
davide <davidecorsinimilano@gmail.com>
2021-02-23 15:21:36 +0100

    end

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        messaggio: 'ciao',
+    }
+})
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,4 @@ let mix = require('laravel-mix');
 mix.js('src/script.js', 'js').sass('src/style.scss', 'css')
 .options({
     processCssUrls: false
-});
+}).vue();
